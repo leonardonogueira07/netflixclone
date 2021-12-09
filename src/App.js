d6b30841a353d108c1d048c1c2103e2e766597a6
Leonardo Nogueira <leojunio007@gmail.com>
2021-12-09 01:57:39 -0300

    netflix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
-function App() {
+export default ()=> {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+
+  useEffect(()=> {
+    const loadAll = async () => {
+      // pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      //Pegar o filme em Destaque
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+      console.log(chosenInfo);
+    }
+
+    loadAll();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="MovieRow">
+        <section className="MovieRow--Area">
+          {featuredData &&
+            <FeaturedMovie item={featuredData} />
+          }
+          <div className="MovieRow--List">
+            {movieList.map((item, key)=>(
+              <div  className="MovieRow--Item">
+                <MovieRow key={key} title={item.title} items={item.items} />
+              </div>
+            ))}
+          </div>          
+        </section>
+      </div>
+    </>
   );
 }
-
-export default App;
